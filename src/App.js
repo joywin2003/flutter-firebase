3811bd2814db868262ff6a050b431104f8e6bce0
joywin2003 <21a20.joywin@sjec.ac.in>
2023-08-07 20:31:58 +0530

    new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './components/AddUser';
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddUser.js
@@ -0,0 +1,14 @@
+function AddUser() {
+    return (
+        <div>
+           <form>
+               <label>Username</label>
+               <input type="text"/>
+               <label>Age(in years)</label>
+               <input type="text"/>
+           </form> 
+        </div>
+    );
+}
+
+export default AddUser;
